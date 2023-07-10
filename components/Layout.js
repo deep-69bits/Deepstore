4b5adbe8d1fb50aa91c0d0d5a392287e567e93cb
deep-69bits <deepakchattwaniking15@gmail.com>
2023-07-10 14:33:11 +0530

    optimization

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,6 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Seo from './Seo'
-import dynamic from 'next/dynamic'
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-  ssr: false
-});
 
 
 
@@ -19,26 +15,7 @@ const Layout = ({ children, products }) => {
       </div>
       <div className='min-h-screen bg-white flex flex-col justify-between'>
         <Header products={products} />
-        <AnimatedCursor
-        outerAlpha={1}
-        innerScale={0.7}
-        outerScale={5}
-        hasBlendMode={true}
-        color='14, 41, 84'
-          clickables={[
-            'a',
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            'label[for]',
-            'select',
-            'textarea',
-            'button',
-            '.link'
-          ]}
-        />
+       
         {
           children
         }
